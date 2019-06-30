8342d2c99daa8c02ef532d2c6e132c0d82d97f39
Congtrieu <ittrjeu@gmail.com>
2019-07-01 01:00:02 +0700

    update

--- src/App.vue
+++ src/App.vue
@@ -1,29 +1,28 @@
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+  <div id="app" class="wrapper">
+    <Header />
+    <Sidebar />
     <router-view />
+    <Footer />
+    <SidebarFooter />
   </div>
 </template>
-
-<style lang="scss">
-#app {
-  font-family: "Avenir", Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+import Header from "@/components/Header.vue";
+import Footer from "@/components/Footer.vue";
+import Sidebar from "@/components/Sidebar.vue";
+import SidebarFooter from "@/components/SidebarFooter.vue";
+// @Component
+@Component({
+  components: {
+    Header,
+    Footer,
+    Sidebar,
+    SidebarFooter
   }
-}
-</style>
+})
+export default class App extends Vue {}
+</script>
+
+<style lang="scss"></style>
---
+++ src/components/Footer.vue
@@ -0,0 +1,12 @@
+<template>
+  <footer class="main-footer">
+    <div class="pull-right hidden-xs">
+      Version
+    </div>
+    <strong>
+      Copyright &copy; 2014-2016
+      <a href="https://adminlte.io">Almsaeed Studio</a>
+    </strong>
+    All rights reserved
+  </footer>
+</template>
