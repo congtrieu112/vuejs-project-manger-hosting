db9a6fd1f3ef9a06fbdef8b5289fb49bdcdac999
Congtrieu <ittrjeu@gmail.com>
2019-07-15 22:41:59 +0700

    update

--- src/views/Mails.vue
+++ src/views/Mails.vue
@@ -30,46 +30,44 @@
             </div>
             <!-- /.box-header -->
             <div class="box-body">
-               <form id="add-or-update-form" role="form" autocomplete="off">
-            <!-- to: req.body.to,//'ittrjeu@gmail.com',
+              <form id="add-or-update-form" role="form" autocomplete="off">
+                <!-- to: req.body.to,//'ittrjeu@gmail.com',
             subject: req.body.subject,//'Test Nodemailer',
             // text: 'You recieved message from ' ,
-            html: req.body.html// -->
-                    <div class="form-group">
-                        <label for="to">To</label>
-                        <input
-                        type="email"
-                        name="to"
-                        class="form-control"
-                        placeholder="admin@sanphamweb.com"
-                        required="required"
-                        />
-                    </div>
-                    <div class="form-group">
-                        <label for="subject">subject</label>
-                        <input
-                        type="text"
-                        name="subject"
-                        class="form-control"
-                        placeholder="Send Test Email"
-                        required="required"
-                        />
-                    </div>
-                    
+                html: req.body.html//-->
+                <div class="form-group">
+                  <label for="to">To</label>
+                  <input
+                    type="email"
+                    name="to"
+                    class="form-control"
+                    placeholder="admin@sanphamweb.com"
+                    required="required"
+                  />
+                </div>
+                <div class="form-group">
+                  <label for="subject">subject</label>
+                  <input
+                    type="text"
+                    name="subject"
+                    class="form-control"
+                    placeholder="Send Test Email"
+                    required="required"
+                  />
+                </div>
 
-                    <div class="form-group">
-                        <label for="description">Description</label>
-                        <textarea
-                        name="description"
-                        class="form-control"
-                        required="required"
-                        ></textarea>
-                    </div>
+                <div class="form-group">
+                  <label for="description">Description</label>
+                  <textarea
+                    id="description"
+                    name="description"
+                    class="form-control"
+                    required="required"
+                  ></textarea>
+                </div>
 
-                   
-                   
                 <button type="submit" class="btn btn-primary">Send</button>
-                </form>
+              </form>
             </div>
             <!-- /.box-body -->
           </div>
@@ -80,7 +78,6 @@
       <!-- /.row -->
     </section>
     <!-- /.content -->
-    
   </div>
 </template>
 <script>
@@ -92,7 +89,7 @@ import Swal from "sweetalert2";
 export default {
   data: function() {
     return {
-      error:true
+      error: true
     };
   },
   methods: {
@@ -102,8 +99,8 @@ export default {
     },
     handleSubmit: async function(e) {
       // this.$refs.form.submit()
-      console.log("this.error",e);
-      if (e.detail.error===false) {
+      console.log("this.error", e);
+      if (e.detail.error === false) {
         const form = $("#add-or-update-form");
         let dataValue = form.serialize();
         const button = form.find("button[type=submit]").button("loading");
@@ -113,7 +110,7 @@ export default {
           Swal.fire(result.data.message, "", "error");
         } else {
           Swal.fire("Send mail success", "", "success").then(result => {
-              form[0].reset();
+            form[0].reset();
           });
         }
       }
@@ -122,77 +119,91 @@ export default {
     }
   },
   mounted() {
-    document.addEventListener('click', this.handleSubmit);
+    document.addEventListener("click", this.handleSubmit);
+    //     $(document).ready(function() {
+    //     $("#add-or-update-form")
+    //         .find('[name="description"]')
+    //         .ckeditor()
+    //         .editor                              // Get the editor instance
+    //             .on('change', function() {      // Called when the value is changed
+    //                 // Revalidate the bio field
+    //                 console.log("change")
+
+    //             });
+    // });
+    var editor = CKEDITOR.replace("description", {
+      removePlugins: "sourcearea"
+    });
     // CKEDITOR.replace('description');
-    // CKEDITOR.editorConfig = function( config ) {
-    //     config.toolbarGroups =  [
-    //             { name: 'document',	   groups: [ 'mode', 'document' ] },			// Displays document group with its two subgroups.
-    //             { name: 'clipboard',   groups: [ 'clipboard', 'undo' ] },			// Group's name will be used to create voice label.
-    //             '/',																// Line break - next group will be placed in new line.
-    //             { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
-    //             { name: 'links' }
-    //         ];
-    // };
+    CKEDITOR.on("instanceReady", function() {
+      $.each(CKEDITOR.instances, function(instance) {
+        CKEDITOR.instances[instance].on("change", function(e) {
+          for (instance in CKEDITOR.instances)
+            CKEDITOR.instances[instance].updateElement();
+        });
+      });
+    });
     $("#add-or-update-form").bootstrapValidator({
-         live: 'enabled',
-        fields: {
-          to: {
-            validators: {
-              notEmpty: {
-                message: "please input email"
-              },
-              emailAddress: {
-                message: "email not correct format"
-              }
+      message: "This value is not valid",
+      excluded: [":disabled"],
+      live: "enabled",
+      fields: {
+        to: {
+          validators: {
+            notEmpty: {
+              message: "please input email"
+            },
+            emailAddress: {
+              message: "email not correct format"
             }
-          },
-          subject: {
-            validators: {
-              notEmpty: {
-                message: "please input subject"
-              }
-            }
-          },
-          description: {
-            validators: {
-              notEmpty: {
-                message: "please input content"
-              },
-              callback: {
-                    message: 'The bio must be less than 200 characters long',
-                    callback: function(value, validator, $field) {
-                        // Get the plain text without HTML
-                        var div  = $('<div/>').html(value).get(0),
-                            text = div.textContent || div.innerText;
-
-                        return text.length <= 200;
-                    }
-                }
-            }
-          },
-          
+          }
         },
-
-        onSuccess:async function(e, data) {
-          e.preventDefault();
-          var event = new CustomEvent("click",{
-            detail: {
-              error:false
+        subject: {
+          validators: {
+            notEmpty: {
+              message: "please input subject"
             }
-          });
-          document.dispatchEvent(event); 
-          return false;
-        }
-      }).find('[name="description"]')
-            .ckeditor()
-            .editor
-                // To use the 'change' event, use CKEditor 4.2 or later
-                .on('change', function() {
-                    // Revalidate the bio field
-                    $('#add-or-update-form').bootstrapValidator('revalidateField', 'description');
-                });
-    
+          }
+        },
+        description: {
+          validators: {
+            notEmpty: {
+              message: "please input description"
+            }
+            // callback: {
+            //     message: 'The bio must be less than 200 characters long',
+            //     callback: function(value, validator, $field) {
+            //         // Get the plain text without HTML
+            //         console.log("value",value)
+            //         console.log("validator",validator)
+            //         console.log("$field",$field)
+            //         var div  = $('<div/>').html(value).get(0),
+            //             text = div.textContent || div.innerText;
 
+            //         return text.length <= 200;
+            //     }
+            // }
+          }
+        }
+      },
+
+      onSuccess: async function(e, data) {
+        e.preventDefault();
+        var event = new CustomEvent("click", {
+          detail: {
+            error: false
+          }
+        });
+        document.dispatchEvent(event);
+        return false;
+      }
+    });
+    editor.on("change", function(ev) {
+      $("#add-or-update-form").bootstrapValidator(
+        "revalidateField",
+        "description"
+      );
+    });
   }
 };
 </script>
